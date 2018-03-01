dd43a43744472050074b12466435916c227a36da
Kiyunna <flouriusk3@gator.uhd.edu>
2018-03-01 16:59:16 -0600

    Learning A5
    Two-Way-Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
   serverName = 'Testserver';
+  ServerCreated = false;
 
   constructor() {
   	setTimeout(() => {
@@ -22,7 +23,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-  	this.serverCreationStatus = 'Server was created';
+  	this.serverCreated = true;
+  	this.serverCreationStatus = 'Server was created Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any) {
